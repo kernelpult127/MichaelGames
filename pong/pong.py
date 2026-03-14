26d90c7e86b790a2df75172f1a47e1eb12746dc3
Michael Pietrucha <Michaelpietruchajr@gmail.com>
2026-03-14 12:58:48 -0500

    added base of game

--- pong/pong.py
+++ pong/pong.py
@@ -1,4 +1,19 @@
-x = 20
-print(x)
-print("Hello World")
-print("Hamza Wali is very nice")
+import pygame
+import sys
+import os
+
+pygame.init()
+
+WIDTH = 1000
+HEIGHT = 700
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Funny Pong")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
